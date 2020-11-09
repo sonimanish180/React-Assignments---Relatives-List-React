c185f7c2893072d1720d399c81002591a376246d
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-11-09 21:29:08 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  render() {
+    const relativeList = [
+      { name: "Ajay", key: "relativeListItem1" },
+      { name: "Pavitra", key: "relativeListItem2" },
+      { name: "Himanshu", key: "relativeListItem3" }
+    ];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol>
+          {relativeList.map((relative) => (
+            <li key={relative.key}>{relative.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
